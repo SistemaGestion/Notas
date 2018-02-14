257e6c7a09e4c5a9a707828a338c1616b9238eb0
Gaston Loyoco <Gaston Loyoco@GASTONLOYOCO-PC>
2018-02-14 10:33:42 -0300

    Final

---
+++ Notas/Controllers/NotaController.cs
@@ -0,0 +1,222 @@
+﻿using Notas.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notas.Controllers
+{
+    public class NotaController : Controller
+    {
+        //String sqlExiste = "SELECT * FROM dbo.Nota WHERE titulo = @titulo AND descripcion = @descripcion";
+        
+        // GET: Nota
+        public ActionResult Index()
+        {
+            using (var db = new ContextNota())
+            {
+                Usuario usuario = (Usuario)Session["Usuario"];
+                List<Nota> notas = db.Nota.Where(a => a.id_usuario == usuario.id).ToList();
+                return View(notas);
+            }
+        }
+
+
+        public ActionResult AgregarNota()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarNota(Nota nota)
+        {
+            if (Session["Usuario"] != null)
+            {
+                Usuario usuario = (Usuario)Session["Usuario"];
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+                else
+                {
+                    string sqlCarga = @"INSERT INTO dbo.Nota (titulo, descripcion, id_usuario) VALUES(@titulo, @descripcion, @id_usuario)";
+
+                    using (var db = new ContextNota())
+                    {
+                        using (SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(db.Database.Connection.ConnectionString))
+                        {
+                            try
+                            {
+                                SqlCommand cmd = new SqlCommand(sqlCarga, sqlConnection1);
+                                cmd.Parameters.AddWithValue("titulo", nota.titulo);
+                                cmd.Parameters.AddWithValue("descripcion", nota.descripcion);
+                                cmd.Parameters.AddWithValue("id_usuario", usuario.id);
+                                sqlConnection1.Open();
+                                cmd.Connection = sqlConnection1;
+                                cmd.ExecuteNonQuery();
+                                sqlConnection1.Close();
+                                return RedirectToAction("Index");
+                            }
+                            catch (Exception)
+                            {
+                                ModelState.AddModelError("", "No se pudo crear la nota.");
+                                return View();
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Ocurrio un error. Intente nuevamente mas tarde.");
+                return View();
+            }
+            
+        }
+
+        public Nota Buscar(int _id)
+        {
+            using (var db = new ContextNota())
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(db.Database.Connection.ConnectionString))
+                {
+                    String sql = "SELECT * FROM dbo.Nota WHERE id = " + _id;
+                    if (sqlConnection != null)
+                    {
+                        SqlCommand cmd = new SqlCommand(sql, sqlConnection);
+                        cmd.Parameters.AddWithValue("id", _id);
+                        try
+                        {
+                            sqlConnection.Open();
+                            SqlDataReader dr = cmd.ExecuteReader();
+                            if (dr.Read())
+                            {
+                                Nota nota = new Nota((int)dr["id"], (string)dr["titulo"], (string)dr["descripcion"], (int)dr["id_usuario"]);
+                                return nota;
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                        catch (SqlException)
+                        {
+                            return null;
+                        }
+                        finally
+                        {
+                            sqlConnection.Close();
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Nota nota = Buscar(id);
+            if (nota !=null)
+            {
+                return View(nota);
+            }
+            else
+            {
+                ModelState.AddModelError("", "No se encontro el usuario correspondiente.");
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Nota _nota)
+        {
+
+            using (var db = new ContextNota())
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(db.Database.Connection.ConnectionString))
+                {
+                    String sqlActualizar = "UPDATE dbo.Nota SET titulo = '" + _nota.titulo + "', descripcion = '" + _nota.descripcion + "' WHERE id = " + _nota.id;
+                    if (sqlConnection != null)
+                    {
+                        SqlCommand cmd = new SqlCommand(sqlActualizar, sqlConnection);
+                        try
+                        {
+                            sqlConnection.Open();
+                            cmd.ExecuteNonQuery();
+                            return RedirectToAction("Index");
+                            
+                        }
+                        catch (SqlException)
+                        {
+                            ModelState.AddModelError("", "No se pudo actualizar.");
+                            return View();
+                        }
+                        finally
+                        {
+                            sqlConnection.Close();
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public ActionResult Ver(int id)
+        {
+            Nota nota = Buscar(id);
+            if (nota != null)
+            {
+                return View(nota);
+            }
+            else
+            {
+                ModelState.AddModelError("", "No se encuentra la nota que esta intendo ver.");
+                return View();
+            }
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            using (var db = new ContextNota())
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(db.Database.Connection.ConnectionString))
+                    {
+                    try
+                    {
+                        Nota nota = Buscar(id);
+                        if (nota != null)
+                        {
+                            String sqlBorrar = "DELETE FROM dbo.Nota WHERE id = " + nota.id;
+                            sqlConnection.Open();
+                            SqlCommand cmd = new SqlCommand(sqlBorrar, sqlConnection);
+                            cmd.ExecuteNonQuery();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index");
+                        }
+                    }
+                    catch (Exception )
+                    {
+                        ModelState.AddModelError("", "No se pudo borrar.");
+                        return RedirectToAction("Index");
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+            }
+        }
+    }    
+}
---
+++ Notas/Controllers/UsuarioController.cs
@@ -0,0 +1,189 @@
+﻿using Notas.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                String sql = @"SELECT * FROM dbo.Usuario WHERE nombre_usuario = '" + user.nombre_usuario + "' AND contraseña = '" + user.contraseña + "'";
+
+                using (var db = new ContextNota())
+                {
+                    using (SqlConnection cn = new System.Data.SqlClient.SqlConnection(db.Database.Connection.ConnectionString))
+                    {
+                        if (cn != null)
+                        {
+                            cn.Open();
+                            SqlCommand cmd = new SqlCommand(sql, cn);
+                            try
+                            {
+                                SqlDataReader dr = cmd.ExecuteReader();
+                                if (dr.Read())
+                                {
+                                    Usuario _user = new Usuario((int)dr["id"], (string)dr["nombre"], (string)dr["apellido"], (Nullable<System.DateTime>)dr["fecha_nacimiento"], (string)dr["email"], (string)dr["nombre_usuario"], (string)dr["contraseña"]);
+                                    Session["Usuario"] = _user;
+                                    return RedirectToAction("../Nota/Index");
+                                }
+                                else
+                                {
+                                    ModelState.AddModelError("", "Usuario y/o contraseña invalido.");
+                                    return View();
+                                }
+                               
+                            }
+                            catch (Exception)
+                            {
+                                ModelState.AddModelError("", "Usuario y/o contraseña invalido.");
+                                return View();
+                            }
+                            finally
+                            {
+                                cn.Close();
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Problemas con la conexion.");
+                            return View();
+                        }
+                    }
+                }
+            }       
+        }  
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public void sugerencia(Usuario _usuario)
+        {
+            List<string> list = new List<string>();
+            list.Add(_usuario.apellido);
+            list.Add(_usuario.nombre);
+            list.Add(_usuario.fecha_nacimiento.Value.Month.ToString());
+            list.Add(_usuario.fecha_nacimiento.Value.Day.ToString());
+            list.Add(_usuario.fecha_nacimiento.Value.DayOfYear.ToString());
+            list.Add(_usuario.fecha_nacimiento.Value.Year.ToString());
+
+            Random random = new Random();
+
+            
+            using (var db = new ContextNota())
+            {
+                using (SqlConnection sqlConnection = new System.Data.SqlClient.SqlConnection(db.Database.Connection.ConnectionString))
+                {
+                    sqlConnection.Open();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string ne = "";
+                        for (int j = 0; j < 3; j++)
+                        {
+                            int pos = random.Next(list.Count);
+                            ne += list[pos];
+                        }
+                        string sqlExiste = @"SELECT COUNT(*) FROM dbo.Usuario WHERE nombre_usuario = '" + ne + "' OR email = '" + _usuario.email+ "'";
+                             SqlCommand cmd = new SqlCommand(sqlExiste, sqlConnection);
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (count == 0)
+                        {
+                            ModelState.AddModelError("", ne);
+                        }
+                        
+
+                    }
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registro(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                string sqlExiste = @"SELECT COUNT(*) FROM dbo.Usuario WHERE nombre_usuario = @nombre_usuario OR email = @email";
+
+                string sqlCarga = @"INSERT INTO dbo.Usuario (nombre, apellido, fecha_nacimiento, email, nombre_usuario, contraseña) VALUES(@nombre, @apellido, @fecha_nacimiento, @email, @nombre_usuario, @contraseña)";
+
+                using (var db = new ContextNota())
+                {
+                    using (SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(db.Database.Connection.ConnectionString))
+                    {
+                        SqlCommand cmd = new SqlCommand(sqlExiste, sqlConnection1);
+                        cmd.Parameters.AddWithValue("nombre_usuario", usuario.nombre_usuario);
+                        cmd.Parameters.AddWithValue("email", usuario.email);
+                        sqlConnection1.Open();
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (count == 0)
+                        {
+                            try
+                            {
+                                SqlCommand command = new SqlCommand(sqlCarga,sqlConnection1);
+                                command.Parameters.Add("@nombre", SqlDbType.VarChar).Value = usuario.nombre;
+                                command.Parameters.Add("@apellido", SqlDbType.VarChar).Value = usuario.apellido;
+                                command.Parameters.Add("@fecha_nacimiento", SqlDbType.DateTime).Value = usuario.fecha_nacimiento;
+                                command.Parameters.Add("@email", SqlDbType.VarChar).Value = usuario.email;
+                                command.Parameters.Add("@nombre_usuario", SqlDbType.VarChar).Value = usuario.nombre_usuario;
+                                command.Parameters.Add("@contraseña", SqlDbType.VarChar).Value = usuario.contraseña;
+                                command.Connection = sqlConnection1;
+                                command.ExecuteNonQuery();
+                                sqlConnection1.Close();
+                                return RedirectToAction("Login");
+                            }
+                            catch (Exception )
+                            {
+                                ModelState.AddModelError("", "No se pudo crear el usuario. Por favor intente mas tarde.");
+                                return View();
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("","El usuario que esta intentando creas ya se encuentra registrado. Intente acceder o con otro usuario.");
+                            sugerencia(usuario);
+                            sqlConnection1.Close();
+                            return View();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
